e90ab41ecb36547d167bbf7a8e2f11ebced812ff
Carlos <carlos.grh@outlook.com>
2022-06-23 09:29:31 -0500

    href url for the different webpages.

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'my_app'
+
 urlpatterns = [
-    path('', views.home),
-    path('recetas-italiana/', views.italiana),
-    path('recetas-mar/', views.mar),
-    path('recetas-colombiana/', views.colombiana),
+    path('', views.home, name='home'),
+    path('recetas-italiana/', views.italiana, name='italiana'),
+    path('recetas-mar/', views.mar, name='mar'),
+    path('recetas-colombiana/', views.colombiana, name='colombiana'),
 ]
