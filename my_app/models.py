8b3b6e52ab320811e314c7bb8b0b588fe07b1d99
Carlos <carlos.grh@outlook.com>
2022-06-23 20:18:04 -0500

    add cols for recipes into homepage

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class RecetasItalianas(models.Model):
+    nombre_plato = models.CharField(max_length=30, unique=True, null=False)
+    tiempo_cocina = models.IntegerField(unique=False, null=False)
+    ingredientes = models.CharField(max_length=100 ,unique=False, null=False)
+    autor = models.CharField(max_length=30, unique=False, null=False)
+    dificultad = models.IntegerField(unique=False, null=False)
+    fecha_agregada = models.DateField(unique=False, null=False)
+
+class RecetasMar(models.Model):
+    nombre_plato = models.CharField(max_length=30, unique=True, null=False)
+    tiempo_cocina = models.IntegerField(unique=False, null=False)
+    ingredientes = models.CharField(max_length=100 ,unique=False, null=False)
+    autor = models.CharField(max_length=30, unique=False, null=False)
+    dificultad = models.IntegerField(unique=False, null=False)
+    fecha_agregada = models.DateField(unique=False, null=False)
+
+class RecetasColombianas(models.Model):
+    nombre_plato = models.CharField(max_length=30, unique=True, null=False)
+    tiempo_cocina = models.IntegerField(unique=False, null=False)
+    ingredientes = models.CharField(max_length=100 ,unique=False, null=False)
+    autor = models.CharField(max_length=30, unique=False, null=False)
+    dificultad = models.IntegerField(unique=False, null=False)
+    fecha_agregada = models.DateField(unique=False, null=False)
