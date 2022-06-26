2e3238fc217a870947816a3210708031dfbf9f50
Carlos <carlos.grh@outlook.com>
2022-06-26 12:41:27 -0500

    form post working and rendering into homepage

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,9 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from requests import patch
 from . import models
-import math
+from datetime import date
 # Create your views here.
 
 
@@ -27,17 +28,44 @@ def home(request):
 
 
 def italiana(request):
+    
     # Test POST
     if request.POST:
-        print(request.POST)
+        today = date.today()
+        receta = request.POST['receta']
+        duracion = int(request.POST['tiempo'])
+        ingredientes = request.POST['ingredientes']
+        autor = request.POST['autor']
+        dificultad = int(request.POST['dificultad'])
+        print(today, receta, duracion, ingredientes, autor, dificultad)
+        models.RecetasItalianas.objects.create(nombre_plato=receta, tiempo_cocina=duracion, ingredientes=ingredientes, autor=autor, dificultad=dificultad, fecha_agregada=today)
+        return redirect(reverse('my_app:home'))
     return render(request, f'{path_template}{templates_my_app[1]}', context=None)
 
 
 def mar(request):
-
+    if request.POST:
+        today = date.today()
+        receta = request.POST['receta']
+        duracion = int(request.POST['tiempo'])
+        ingredientes = request.POST['ingredientes']
+        autor = request.POST['autor']
+        dificultad = int(request.POST['dificultad'])
+        print(today, receta, duracion, ingredientes, autor, dificultad)
+        models.RecetasMar.objects.create(nombre_plato=receta, tiempo_cocina=duracion, ingredientes=ingredientes, autor=autor, dificultad=dificultad, fecha_agregada=today)
+        return redirect(reverse('my_app:home'))
     return render(request, f'{path_template}{templates_my_app[2]}', context=None)
 
 
 def colombiana(request):
-
+    if request.POST:
+        today = date.today()
+        receta = request.POST['receta']
+        duracion = int(request.POST['tiempo'])
+        ingredientes = request.POST['ingredientes']
+        autor = request.POST['autor']
+        dificultad = int(request.POST['dificultad'])
+        print(today, receta, duracion, ingredientes, autor, dificultad)
+        models.RecetasColombianas.objects.create(nombre_plato=receta, tiempo_cocina=duracion, ingredientes=ingredientes, autor=autor, dificultad=dificultad, fecha_agregada=today)
+        return redirect(reverse('my_app:home'))
     return render(request, f'{path_template}{templates_my_app[3]}', context=None)
