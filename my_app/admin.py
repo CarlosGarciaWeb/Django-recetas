1aad24e00058465dbd0887593fa512f2b12a827b
Carlos <carlos.grh@outlook.com>
2022-06-26 14:10:47 -0500

    install models into admin.

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from my_app.models import RecetasItalianas, RecetasColombianas, RecetasMar
 # Register your models here.
+
+
+class RecetasAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register([RecetasItalianas, RecetasMar, RecetasColombianas])
