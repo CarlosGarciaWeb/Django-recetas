c38fde521be58dee650a8119b8a62c5eb3585dfc
Carlos <carlos.grh@outlook.com>
2022-06-26 09:14:32 -0500

    Render Db information into html pages

--- my_app/models.py
+++ my_app/models.py
@@ -1,27 +1,36 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class RecetasItalianas(models.Model):
     nombre_plato = models.CharField(max_length=30, unique=True, null=False)
-    tiempo_cocina = models.IntegerField(unique=False, null=False)
+    tiempo_cocina = models.IntegerField(unique=False, null=False, validators=[MinValueValidator(30), MaxValueValidator(24)])
     ingredientes = models.CharField(max_length=100 ,unique=False, null=False)
     autor = models.CharField(max_length=30, unique=False, null=False)
-    dificultad = models.IntegerField(unique=False, null=False)
+    dificultad = models.IntegerField(unique=False, null=False, validators=[MinValueValidator(1), MaxValueValidator(5)])
     fecha_agregada = models.DateField(unique=False, null=False)
+
+    def __str__(self):
+        return f"{self.nombre_plato} , {self.tiempo_cocina}, {self.ingredientes}, {self.autor}, {self.dificultad}, {self.fecha_agregada}"
 
 class RecetasMar(models.Model):
     nombre_plato = models.CharField(max_length=30, unique=True, null=False)
-    tiempo_cocina = models.IntegerField(unique=False, null=False)
+    tiempo_cocina = models.IntegerField(unique=False, null=False, validators=[MinValueValidator(30), MaxValueValidator(24)])
     ingredientes = models.CharField(max_length=100 ,unique=False, null=False)
     autor = models.CharField(max_length=30, unique=False, null=False)
-    dificultad = models.IntegerField(unique=False, null=False)
+    dificultad = models.IntegerField(unique=False, null=False, validators=[MinValueValidator(1), MaxValueValidator(5)])
     fecha_agregada = models.DateField(unique=False, null=False)
+
+    def __str__(self):
+        return f"{self.nombre_plato} , {self.tiempo_cocina}, {self.ingredientes}, {self.autor}, {self.dificultad}, {self.fecha_agregada}"
 
 class RecetasColombianas(models.Model):
     nombre_plato = models.CharField(max_length=30, unique=True, null=False)
-    tiempo_cocina = models.IntegerField(unique=False, null=False)
+    tiempo_cocina = models.IntegerField(unique=False, null=False, validators=[MinValueValidator(30), MaxValueValidator(24)])
     ingredientes = models.CharField(max_length=100 ,unique=False, null=False)
     autor = models.CharField(max_length=30, unique=False, null=False)
-    dificultad = models.IntegerField(unique=False, null=False)
+    dificultad = models.IntegerField(unique=False, null=False, validators=[MinValueValidator(1), MaxValueValidator(5)])
     fecha_agregada = models.DateField(unique=False, null=False)
+
+    def __str__(self):
+        return f"{self.nombre_plato} , {self.tiempo_cocina}, {self.ingredientes}, {self.autor}, {self.dificultad}, {self.fecha_agregada}"
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from requests import patch
-
+from . import models
 # Create your views here.
 
 
@@ -11,18 +11,38 @@ templates_my_app = ['index.html', 'italiana.html', 'mar.html', 'colombiana.html'
 ## Render Templates
 
 def home(request):
-    return render(request, f'{path_template}{templates_my_app[0]}')
+    recetas_italianas = models.RecetasItalianas.objects.all()
+    recetas_mar = models.RecetasMar.objects.all()
+    recetas_colombianas = models.RecetasColombianas.objects.all()
+    context = {
+        'italianas': recetas_italianas,
+        'mar': recetas_mar,
+        'colombianas': recetas_colombianas
+    }
+    return render(request, f'{path_template}{templates_my_app[0]}', context=context)
 
 
 
 
 def italiana(request):
-    return render(request, f'{path_template}{templates_my_app[1]}')
+    recetas_italianas = models.RecetasItalianas.objects.all()
+    context = {
+        'italianas': recetas_italianas
+    }
+    return render(request, f'{path_template}{templates_my_app[1]}', context=context)
 
 
 def mar(request):
-    return render(request, f'{path_template}{templates_my_app[2]}')
+    recetas_mar = models.RecetasMar.objects.all()
+    context = {
+        'mar': recetas_mar
+    }
+    return render(request, f'{path_template}{templates_my_app[2]}', context=context)
 
 
 def colombiana(request):
-    return render(request, f'{path_template}{templates_my_app[3]}')
+    recetas_colombianas = models.RecetasColombianas.objects.all()
+    context = {
+        'colombianas': recetas_colombianas
+    }
+    return render(request, f'{path_template}{templates_my_app[3]}', context=context)
